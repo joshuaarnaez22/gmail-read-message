2c9230129243eb9842bb42f6fb6872faf08103ff
Joshua V Arnaez <jarnaez@yondu.com>
2023-03-24 10:00:17 +0800

    first push

---
+++ node/index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const cors = require("cors");
+
+const { google } = require("googleapis");
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = "http://localhost:3000/callback";
+
+const port = 3001;
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+
+app.get("/auth", (req, res) => {
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: [
+      "https://www.googleapis.com/auth/gmail.readonly",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  });
+  //issue with google api cors should be (res.redirect) instead of res.send
+  res.send(authUrl);
+});
+
+app.post("/signout", async (req, res) => {
+  try {
+    const { token } = req.query;
+    const response = await oauth2Client.revokeToken(token);
+    console.log("Token revoked:", response.data);
+    res.send();
+  } catch (error) {
+    console.error("Error revoking token:", error.message);
+  }
+});
+
+app.get("/callback", async (req, res) => {
+  try {
+    const { code } = req.query;
+    const { tokens } = await oauth2Client.getToken(code);
+    oauth2Client.setCredentials(tokens);
+
+    //get the messages in the authenticated user
+    const gmail = google.gmail({ version: "v1", auth: oauth2Client });
+    const {
+      data: { emailAddress },
+    } = await gmail.users.getProfile({ userId: "me" });
+    const {
+      data: { messages },
+    } = await gmail.users.messages.list({ userId: "me" });
+
+    let fullMessages = [];
+
+    //loop all the messageList to get additional data
+    for (const message of messages) {
+      const fullMessage = await gmail.users.messages.get({
+        userId: "me",
+        id: message.id,
+        format: "full",
+      });
+
+      const headers = fullMessage.data.payload.headers;
+      const messageData = {
+        id: message.id,
+        content: fullMessage.data.snippet,
+        subject: headers.find((header) => header.name === "Subject").value,
+        sender: headers.find((header) => header.name === "From").value,
+        receiver: headers.find((header) => header.name === "To").value,
+        date: headers.find((header) => header.name === "Date").value,
+      };
+      fullMessages.push({ ...messageData });
+    }
+
+    res.send({ emailAddress, fullMessages, token: tokens.access_token });
+  } catch (error) {
+    // console.error("Error retrieving access token:", error);
+    res.status(500).send("Error retrieving access token");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+interface Message {
+  email: string;
+  token: string;
+  fullMessages: FullMessage[];
+}
 
+interface FullMessage {
+  id: string;
+  content: string;
+  date: string;
+  receiver: string;
+  sender: string;
+  subject: string;
+}
 function App() {
+  const [messages, setMessages] = useState<Message>();
+  const [auth, setAuth] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const signInHandler = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/auth");
+      const url = await response.text();
+      window.location.href = url;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const signOutHandler = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3001/signout?token=${messages?.token}`,
+        { method: "POST" }
+      );
+      if (response.status === 200)
+        window.location.href = "http://localhost:3000";
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const code = urlSearchParams.get("code");
+    if (code) {
+      (async () => {
+        try {
+          setLoading(true);
+          const res = await fetch(
+            `http://localhost:3001/callback?code=${code}`
+          );
+          const message = await res.json();
+          setLoading(false);
+          setMessages(message);
+          setAuth(true);
+        } catch (error) {
+          setAuth(false);
+          window.location.href = "http://localhost:3000";
+          console.log(error);
+        }
+      })();
+    }
+  }, []);
+
+  //should add a custom loader
+  if (loading)
+    return (
+      <>
+        <p>Loading messages...</p>
+      </>
+    );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Note!! this app does not save token yet so refresh is redirected to sign
+        in
+      </p>
+      <main>
+        {!auth ? (
+          <button onClick={signInHandler}>Login with Google</button>
+        ) : (
+          <button onClick={signOutHandler}>SignOut</button>
+        )}
+      </main>
+
+      <section>
+        {messages &&
+          messages.fullMessages.map((message) => <Card {...message} />)}
+      </section>
     </div>
   );
 }
 
+//should be put in a component folder to be reused
+const Card = ({
+  id,
+  subject,
+  date,
+  sender,
+  receiver,
+  content,
+}: FullMessage) => {
+  return (
+    <div className="card" key={id}>
+      <div className="card-header">
+        <div className="card-title">{subject}</div>
+        <div className="card-date">{date}</div>
+      </div>
+      <div className="card-body">
+        <div className="card-sender">From: {sender}</div>
+        <div className="card-receiver">To: {receiver}</div>
+        <div className="card-content">{content}</div>
+      </div>
+    </div>
+  );
+};
 export default App;
